25b0ec2313f249a6ae2406aabb73d7454882c45d
GeKorm <GeKorm@users.noreply.github.com>
2014-10-24 20:39:44 +0300

    Resolved jQuery conflict
    Attempting to resolve by replacing $ with "jQuery"

--- demo.js
+++ demo.js
@@ -1,17 +1,19 @@
-var $microsecondsDsp = $('#microsecondsDsp');
-var $millisecondsDsp = $('#millisecondsDsp');
-var $secondsDsp = $('#secondsDsp');
-var $minutesDsp = $('#minutesDsp');
-var $hoursDsp = $('#hoursDsp');
+jQuery(document).ready(function() {
+
+var microsecondsDsp = jQuery('#microsecondsDsp');
+var millisecondsDsp = jQuery('#millisecondsDsp');
+var secondsDsp = jQuery('#secondsDsp');
+var minutesDsp = jQuery('#minutesDsp');
+var hoursDsp = jQuery('#hoursDsp');
 
 function startDoc() {
-    $('#start-button').css('font-weight', 'bold');
-    $('#hold-button').removeClass('button');
-    $('#hold-button').addClass('disabled');
-    $('#pause-button').removeClass('button');
-    $('#pause-button').addClass('disabled');
-    $('#pause-button').css('outline', 'none');
-    $('#pause-button').text('Pause');
+    jQuery('#start-button').css('font-weight', 'bold');
+    jQuery('#hold-button').removeClass('button');
+    jQuery('#hold-button').addClass('disabled');
+    jQuery('#pause-button').removeClass('button');
+    jQuery('#pause-button').addClass('disabled');
+    jQuery('#pause-button').css('outline', 'none');
+    jQuery('#pause-button').text('Pause');
 }
 
 startDoc();
@@ -21,15 +23,15 @@ var startOnce = false;
 
 
 
-$('#start-button').on('click', function () {
-    $('#start-button').css('font-weight', '300');
-    $('#start-button').removeClass('button');
-    $('#start-button').addClass('disabled');
-    $('#start-button').css('outline', 'rgba(235, 169, 0, 0.25) dashed 1px');
-    $('#pause-button').addClass('button');
-    $('#pause-button').removeClass('disabled');
-    $('#pause-button').css('outline', 'none');
-    $('#pause-button').text('Pause');
+jQuery('#start-button').on('click', function () {
+    jQuery('#start-button').css('font-weight', '300');
+    jQuery('#start-button').removeClass('button');
+    jQuery('#start-button').addClass('disabled');
+    jQuery('#start-button').css('outline', 'rgba(235, 169, 0, 0.25) dashed 1px');
+    jQuery('#pause-button').addClass('button');
+    jQuery('#pause-button').removeClass('disabled');
+    jQuery('#pause-button').css('outline', 'none');
+    jQuery('#pause-button').text('Pause');
     hey.start();
     startOnce = true;
     pauseCount = 0;
@@ -40,27 +42,29 @@ $('#start-button').on('click', function () {
     hey.display('#microsecondsDsp', 'microseconds');
 });
 
-$('#pause-button').on('click', function () {
+jQuery('#pause-button').on('click', function () {
     if (startOnce === true) {
-        $('#start-button').addClass('button');
-        $('#start-button').removeClass('disabled');
-        $('#start-button').css('outline', 'none');
-        if ($('#start-button').css('font-weight') != 'bold') {
-            $('#pause-button').text('Reset');
-            $('#pause-button').css('outline', 'rgba(235, 169, 0, 0.25) dashed 1px');
+        jQuery('#start-button').addClass('button');
+        jQuery('#start-button').removeClass('disabled');
+        jQuery('#start-button').css('outline', 'none');
+        if (jQuery('#start-button').css('font-weight') != 'bold') {
+            jQuery('#pause-button').text('Reset');
+            jQuery('#pause-button').css('outline', 'rgba(235, 169, 0, 0.25) dashed 1px');
         }
         hey.pause();
         if (pauseCount >= 1) {
             hey.reset();
-            $microsecondsDsp.text(0);
-            $millisecondsDsp.text(0);
-            $secondsDsp.text(0);
-            $minutesDsp.text(0);
-            $hoursDsp.text(0);
-            $('#pause-button').css('outline', 'none');
-            $('#pause-button').text('Pause');
+            microsecondsDsp.text(0);
+            millisecondsDsp.text(0);
+            secondsDsp.text(0);
+            minutesDsp.text(0);
+            hoursDsp.text(0);
+            ('#pause-button').css('outline', 'none');
+            ('#pause-button').text('Pause');
             startDoc();
         }
         pauseCount += 1;
     }
 });
+});
+
